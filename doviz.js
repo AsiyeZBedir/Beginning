02f205b559d3093db1128784974f458a4df643d7
Asiye <116940286+liaail@users.noreply.github.com>
2023-08-30 23:37:22 +0300

    Create doviz.js

---
+++ doviz.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Döviz Dönüştürücü</title>
+</head>
+<body>
+    <h1>Döviz Dönüştürücü</h1>
+
+    <label for="amount">Miktar:</label>
+    <input type="number" id="amount" placeholder="Miktarı girin">
+    
+    <label for="fromCurrency">Dönüştürülecek döviz:</label>
+    <select id="fromCurrency">
+        <option value="usd">USD - Amerikan Doları</option>
+        <option value="eur">EUR - Euro</option>
+        <option value="gbp">GBP - İngiliz Sterlini</option>
+    </select>
+    
+    <label for="toCurrency">Dönüştürülecek döviz:</label>
+    <select id="toCurrency">
+        <option value="us
